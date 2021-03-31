b8c82f33c68d24763798ba37f64a05addaf898e7
takashi-Q <79442863+takashi-Q@users.noreply.github.com>
2021-03-31 19:41:13 +0900

    正規表現に基づいたバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :email , presence: true
-         validates :encrypted_password , presence: true
-         validates :first_name , presence: true
-         validates :first_name_kana , presence: true
-         validates :last_name , presence: true
-         validates :last_name_kana , presence: true
-         validates :date , presence: true
+         with_options presence: true do
+         validates :name,format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
+         validates :email
+         validates :encrypted_password 
+         validates :first_name,format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
+         validates :first_name_kana,format:{ with: /\A[ァ-ヶー－]+\z/ } 
+         validates :last_name,format: { with: /\A[ぁ-んァ-ン一-龥々]/ } 
+         validates :last_name_kana,format:{ with: /\A[ァ-ヶー－]+\z/ }
+         validates :date 
+      end
 
         
 
