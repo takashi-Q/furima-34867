e29c4574c8c26a44d7479d9163ff6d76212612f8
takashi-Q <79442863+takashi-Q@users.noreply.github.com>
2021-04-03 11:03:15 +0900

    ユーザー管理機能　５

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   def index
   end
 
-  def create
-    @Items = Item.find(params[:id])
-  end
+  # def create
+  #   @Items = Item.find(params[:id])
+  # end
 end
