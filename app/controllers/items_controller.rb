d23e31b28631ca472deae09e432d9e9c82d113d2
takashi-Q <79442863+takashi-Q@users.noreply.github.com>
2021-04-09 18:34:16 +0900

    商品一覧表示機能　２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,14 +3,14 @@ class ItemsController < ApplicationController
     @item = Item.all.order('created_at DESC')
   end
 
-  def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
+  # def create
+  #   @item = Item.new(item_params)
+  #   if @item.save
+  #     redirect_to root_path
+  #   else
+  #     render :new
+  #   end
+  # end
 
   def new
     @item = Item.new
