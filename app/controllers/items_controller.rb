aa0548605bb2b5abaf7fc0f2eb5030f522eca595
takashi-Q <79442863+takashi-Q@users.noreply.github.com>
2021-04-10 20:44:08 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:edit, :new]
+  before_action :authenticate_user!, only: [:edit, :update, :destroy, :new]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -16,16 +16,31 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-  
+
   def show
     @item = Item.find(params[:id])
-  
- end
+  end
 
-private
-
-def item_params
-  params.require(:item).permit(:image, :product_name, :description, :category_id, :status_id, :burden_id, :area_id, :days_ship_id,
-                              :price).merge(user_id: current_user.id)
+  def edit
+    @item = Item.find(params[:id])
+    if @item.user != current_user
+      redirect_to root_path
+  end
 end
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :product_name, :description, :category_id, :status_id, :burden_id, :area_id, :days_ship_id,
+                                 :price).merge(user_id: current_user.id)
+  end
 end
