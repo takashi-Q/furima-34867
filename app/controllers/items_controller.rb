48d71b8357903fe672d93193d3fab662bb4acc32
takashi-Q <79442863+takashi-Q@users.noreply.github.com>
2021-04-10 22:09:18 +0900

    商品情報編集機能　２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:edit, :update, :destroy, :new]
+  before_action :authenticate_user!, only: [:edit, :new]
+  before_action :set_item, only: [:edit, :show, :update]
+  before_action :redirect_method, only: [:edit, :update]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -18,23 +20,14 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    if @item.user != current_user
-      redirect_to root_path
   end
-end
+
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
-    if @item.save
-      redirect_to root_path
-    else
-      render :new
-    end
+    redirect_to root_path
   end
 
   private
@@ -42,5 +35,13 @@ end
   def item_params
     params.require(:item).permit(:image, :product_name, :description, :category_id, :status_id, :burden_id, :area_id, :days_ship_id,
                                  :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def redirect_method
+    redirect_to root_path if @item.user != current_user
   end
 end
