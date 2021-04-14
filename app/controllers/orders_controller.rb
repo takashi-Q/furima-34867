2d6926b8e5ac7fc5f1e51f549cb2dd542d28d932
takashi-Q <79442863+takashi-Q@users.noreply.github.com>
2021-04-14 19:44:04 +0900

    商品購入機能　３

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,7 +11,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
 
     if @orderpurchaser.valid?
-      Payjp.api_key = 'sk_test_0efe6a61722aa95c64e454cc' 
+      Payjp.api_key = ENV['PAYJP_SECRET_KEY']
       Payjp::Charge.create(
         amount: @item.price,
         card: orderpurchaser_params[:token], 
